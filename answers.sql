25154c038aab3bfe2aad034f55bfa98f13189880
OnyieBon <onyiebonventure@gmail.com>
2025-05-09 11:12:49 +0300

    Database Design and Normalization

---
+++ answers.sql
@@ -0,0 +1,22 @@
+-- Q1
+
+SELECT 101 AS OrderID, 'John Doe' AS CustomerName, 'Laptop' AS Product
+UNION ALL
+SELECT 101, 'John Doe', 'Mouse'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Tablet'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Keyboard'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Mouse'
+UNION ALL
+SELECT 103, 'Emily Clark', 'Phone';
+
+-- Q2
+-- table1
+SELECT DISTINCT OrderID, CustomerName
+FROM OrderDetails;
+
+-- table2
+SELECT OrderID, Product, Quantity
+FROM OrderDetails;
